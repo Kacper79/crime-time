f89949c50de9fcc9dff52e7c20c3ed074ae5a220
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-04-06 21:42:41 +0200

    Try to make object interactive(Failed)
    Try to make object interactive(Failed cuz i don't have idea how to do it)

---
+++ Assets/Scripts/DisplayPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayPanel : MonoBehaviour {
+	public GameObject panel;
+	public Camera c;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if(Input.GetMouseButtonDown(0)){
+		//	Vector3 mp = c.ScreenToWorldPoint (Input.mousePosition);
+		//	RectTransform r = panel.GetComponent<RectTransform> ();
+		//	if(r.)
+		//}
+	}
+
+	void enable(){
+		panel.activeInHierarchy = true;
+	}
+		
+}
