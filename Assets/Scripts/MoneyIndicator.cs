a4e295282028d43444631a85f54859fb3b109c8f
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-04-06 20:30:15 +0200

    Added basic economy & GUI
    Added Economy with working RT Money counter

---
+++ Assets/Scripts/MoneyIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyIndicator : MonoBehaviour {
+
+	// Use this for initialization
+	public int cash;
+	public Text text;
+
+	void Start () {
+		text.text = "$0";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "" + cash;
+	}
+	void increaseCash(int amount){
+		cash = cash + amount;
+	}
+	void decreaseCash(int amount){
+		cash = cash - amount;
+	}
+}
