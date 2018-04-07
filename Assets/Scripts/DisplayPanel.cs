2f3f7a83dfe9b54277f58d05d536738257032d2b
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-04-07 12:16:17 +0200

    Try to make object interactable 2(Failed)
    The second attempt of making object interactable but it failed

--- Assets/Scripts/DisplayPanel.cs
+++ Assets/Scripts/DisplayPanel.cs
@@ -4,24 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class DisplayPanel : MonoBehaviour {
-	public GameObject panel;
-	public Camera c;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		//if(Input.GetMouseButtonDown(0)){
-		//	Vector3 mp = c.ScreenToWorldPoint (Input.mousePosition);
-		//	RectTransform r = panel.GetComponent<RectTransform> ();
-		//	if(r.)
-		//}
-	}
 
-	void enable(){
-		panel.activeInHierarchy = true;
+	public static void enable(GameObject panel){
+		panel.SetActive (true);
+	}
+	public static void disable(GameObject panel){
+		panel.SetActive (false);
 	}
 		
 }
