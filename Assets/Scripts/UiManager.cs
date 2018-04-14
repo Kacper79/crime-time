56652c9434f850601354429bd3a38730736c5b2e
Kot Mruczyslaw <1standrekw@gmail.com>
2018-04-14 17:15:10 +0200

    Tried to finish UI but Brain broke

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -6,11 +6,17 @@ public class UiManager : MonoBehaviour {
 
 	FlatMechanics Building;
 	public GameObject flatUI;
-
+	public Text[] FlatTexts;
+	public GameObject currentBuilding;
 	public static UiManager Instance;
 
 	void Start () {
 		Instance = this;
+		FlatTexts = flatUI.GetComponentsInChildren <Text>();
+
+		FlatTexts [4].text = "Upgrade Size";
+		FlatTexts [5].text = "Upgrade Equipment";
+		FlatTexts [6].text = "Rent Criminal";
 	}
 	
 	
