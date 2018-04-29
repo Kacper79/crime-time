6240285a80d81830dbdd9e55f344f4846e97bdb6
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-04-29 10:41:39 +0200

    Web Panel(Unfinished)
    Added a example job to jobs panel in web panel.
    TODO: Add generation script for genrating jobs with diffrent prices,names,types and you can change the user icon it's anoying

---
+++ Assets/Scripts/WebJob.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebJob : MonoBehaviour {
+
+    // Use this for initialization
+    string clientName;
+    bool done;
+    public enum JOB_TYPE { Criminal_house,Rent_house};
+
+    public JOB_TYPE Jobtype { get; set; }
+    public int price;
+
+    public void ConstructUI()
+    {
+
+    }
+}
