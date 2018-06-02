4edfd71961bccc073c835197257543330da12e46
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-06-02 15:33:50 +0200

    Date is now displayed in nicer form
    Date is now displayed in a panel instead of single text field.
    Added method to self construct elements of jobs panel (jobs) with given data

---
+++ Assets/Scripts/JobPanel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JobPanel : MonoBehaviour {
+
+    //Scripts that generate Jobs in jobs panel
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
