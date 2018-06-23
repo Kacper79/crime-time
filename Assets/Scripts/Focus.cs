4b901643377fb40f5ea4d127be6d81dc3b4efe7a
Kacper79 <38113297+Kacper79@users.noreply.github.com>
2018-06-23 14:00:16 +0200

    Graphic Refresh 2nd Commit and Identity List
    Added 200 Identities and refreshed design

--- Assets/Scripts/Focus.cs
+++ Assets/Scripts/Focus.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Focus : MonoBehaviour {
 
-    public int state = 0;//0-Game 1-Web
+    public int state = 0;//0-Game 1-BigPanels 2 D1 3 D2
 
     public void setState(int state)
     {
