cd6a339e5bcd6ad903ecee0bda6e6d2a33014188
KotMruczyslaw <1standrewk@gmail.com>
2018-07-22 16:01:30 +0200

    made changes

---
+++ Assets/Scripts/GenerateJobs.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using TMPro.EditorUtilities;
+public class GenerateJobs : MonoBehaviour
+{
+
+	public GameObject job;
+	public Canvas canva;
+	void Start()
+	{
+		MakeJobs(1);
+		MakeJobs(2);
+		MakeJobs(3);
+
+	}
+
+
+	void Update()
+	{
+
+	}
+
+	private void MakeJobs(float offsetY)
+	{
+		Debug.Log("Making Job");
+		GameObject currentJob = Instantiate(job) as GameObject;
+		RectTransform jobTransform = currentJob.GetComponent<RectTransform>();
+		WebJob webjob = currentJob.GetComponent<WebJob>();
+		webjob.clientName = "D'Es Pacito";
+		webjob.Jobtype = JOB_TYPE.Criminal_house;
+		webjob.price = 69;
+		webjob.panel = job;
+		currentJob.transform.parent = gameObject.transform;
+		float transitionX = gameObject.GetComponent<RectTransform>().position.x - jobTransform.position.x;
+		float transitionY = gameObject.GetComponent<RectTransform>().position.y - jobTransform.position.y - jobTransform.sizeDelta.y * offsetY;
+		Vector3 transition = new Vector3(transitionX, transitionY, 0);
+		jobTransform.Translate(transition);
+		jobTransform.offsetMin = new Vector2(0, jobTransform.offsetMin.y);
+		jobTransform.offsetMin = new Vector2(2, jobTransform.offsetMax.y);
+	}
+}
--- Assets/Scripts/WebJob.cs
+++ Assets/Scripts/WebJob.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using TMPro.EditorUtilities;
 
 public class WebJob : MonoBehaviour
 {
@@ -14,15 +16,24 @@ public class WebJob : MonoBehaviour
 	public JOB_TYPE Jobtype;
 	public int price;
 
-	public GameObject proto;
+	public GameObject panel;
+
+	void Start()
+	{
+		Debug.Log("Start");
+		ConstructUI();
+	}
 
 	public void ConstructUI()
 	{
-		GameObject g = Instantiate(proto);
-		Text[] textes = g.GetComponentsInChildren<Text>();
-		textes[0].text = clientName;
-		textes[1].text = Jobtype.ToString();
-		textes[2].text = price.ToString();
+		TMP_Text[] texts = panel.GetComponentsInChildren<TMP_Text>();
+
+		//GameObject g = Instantiate(proto);
+
+		//Text[] texts = panel.GetComponentsInChildren<Text>();
+		texts[0].text = clientName;
+		texts[1].text = Jobtype.ToString();
+		texts[2].text = price.ToString();
 	}
 }
 public enum JOB_TYPE
