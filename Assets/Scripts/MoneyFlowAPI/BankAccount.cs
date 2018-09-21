87915f3c5e22df27b1f08e74082362804cd4784f
Kacper79 <kacper@smykla.pl>
2018-09-22 00:59:34 +0200

    Tweak 1.7a-b

--- Assets/Scripts/MoneyFlowAPI/BankAccount.cs
+++ Assets/Scripts/MoneyFlowAPI/BankAccount.cs
@@ -8,6 +8,7 @@ public class BankAccount : MonoBehaviour
 	public bool accountBlocked;
 	public int money;
 	public List<Law> laws = new List<Law>();
+	public People owner;
     
 	public void TransferMoneyWithoutTaxes(BankAccount recieve,String reason,int money){
 		if (this.money >= money)
@@ -31,6 +32,41 @@ public class BankAccount : MonoBehaviour
 		history.taxPayed = false;
 		TransferRegister.Instance.transfers.Add(history);
 	}
+	public void TransferMoneyWithTaxes(BankAccount recieve, String reason, int money)
+    {
+		MoneyTransferTaxLaw law = new MoneyTransferTaxLaw();
+        TransferHistory history = new TransferHistory
+        {
+            sender = this,
+            reciver = recieve,
+            title = reason,
+            money = money,
+            ID = TransferRegister.Instance.GenerateTransactionID()
+        };
+		history.tax = CalculateTaxes(history, law);
+		if (this.money >= (money+history.tax))
+        {
+			this.money -= (money+history.tax);
+			history.taxPayed = true;
+			TransferHistory taxHistory = new TransferHistory
+			{
+				sender = this,
+				reciver = GovermentAccounts.Instance.transferTaxAccount,
+				ID = TransferRegister.Instance.GenerateTransactionID(),
+				money = history.tax
+			};
+			taxHistory.title = "MTT MTRANSFER " + taxHistory.ID + " FROM " + owner.ID;
+			bankStatements.Add(history);
+			bankStatements.Add(taxHistory);
+            laws.Add(law);
+            TransferRegister.Instance.transfers.Add(history);
+        }
+        else
+        {
+            return;
+        }
+        
+    }
 	public int CalculateTaxes(TransferHistory history,MoneyTransferTaxLaw law){
 		return (int)law.percent * history.money;
 	}
